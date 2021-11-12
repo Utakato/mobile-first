8a569ca01553c36f7d1492f520f8ad99ed6d5e0b
Utakato <copyresearch98@gmail.com>
2021-11-12 13:01:52 +0200

    Edit line to stop at last card

--- script.js
+++ script.js
@@ -6,25 +6,28 @@ const cards = qs(".card", true)
 const d = document.documentElement;
 
 const testing = () => {
-    
     const timelineRect = timeline.getBoundingClientRect();
     const targetY = window.innerHeight * .8;
     const dist = targetY - timelineRect.top;
 
     // This makes the line extend.
-    line.style.height = `${dist}px`
+    if(dist < 4399) {
+        line.style.height = `${dist}px`
+    }else {
+        line.style.height = `4445px`
+    }
 
     // This makes the cards appear/dissapear
     cards.forEach(item => {
         const rect = item.getBoundingClientRect();
-    
-        if (rect.top + item.offsetHeight / 5 < targetY) {
+        if (rect.top + item.offsetHeight / 20 < targetY * 5) {
           item.classList.add('visible');
         }
         if (rect.top + item.offsetHeight / 5 > targetY) {
             item.classList.remove('visible');
           }
       });
+
 };
 
 window.addEventListener("scroll", testing)
