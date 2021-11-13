a6ebee265b7bd1b203228bc2300f3b5ae7be66a0
Utakato <copyresearch98@gmail.com>
2021-11-13 17:15:57 +0200

    Add responsive style

--- script.js
+++ script.js
@@ -9,18 +9,25 @@ const testing = () => {
     const timelineRect = timeline.getBoundingClientRect();
     const targetY = window.innerHeight * .8;
     const dist = targetY - timelineRect.top;
-
+    
     // This makes the line extend.
-    if(dist < 4399) {
+    if( window.innerWidth < 768) {
+        if(dist < 4399) {
+            line.style.height = `${dist}px`
+        }else {
+            line.style.height = `4445px`
+        }
+    }else if (dist < 4025) {
         line.style.height = `${dist}px`
     }else {
-        line.style.height = `4445px`
+        line.style.height = `4025px`
     }
+    console.log(dist)
 
     // This makes the cards appear/dissapear
     cards.forEach(item => {
         const rect = item.getBoundingClientRect();
-        if (rect.top + item.offsetHeight / 20 < targetY * 5) {
+            if (rect.top + item.offsetHeight *0.1  < targetY) {
           item.classList.add('visible');
         }
         if (rect.top + item.offsetHeight / 5 > targetY) {
